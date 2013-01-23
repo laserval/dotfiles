2c0184fe5e77594ed4f975a63f662bbe250e0625
Olof Nilsson <olof.nilsson@findwise.com>
2013-01-23 10:20:53 +0100

    Adds prompt scripts.

---
+++ bash_colors.bash
@@ -0,0 +1,14 @@
+# Colors
+export CLICOLOR=1
+export LSICOLORS=Gxfxcxdxbxegedabagacad
+export TERM="xterm-256color"
+DEFAULT="\e[0m"
+DEFAULTb="\e[1m"
+WHITEb="\e[1;37m"
+GRAY="\e[0;37m"
+CYAN="\e[0;36m"
+BLUE="\e[0;34m"
+YELLOW="\e[0;33m"
+GREEN="\e[0;32m"
+RED="\e[1;31m"
+alias grep='grep --color=auto'
---
+++ bash_ps1.bash
@@ -0,0 +1,31 @@
+# Git-completion PS1 handler
+# Special case for master!
+gitbranch() {
+	local branch="$(__git_ps1 '%s')"
+	case $branch in
+		master*)
+			echo "| 💀  $branch"
+			;;
+		"")
+			echo ""
+			;;
+		*)
+			echo "| $branch"
+			;;
+	esac
+}
+
+# Approval PS1, from http://jamiedubs.com/ps1-collection-customize-your-bash-prompt
+approval() {
+	if [ \$? = 0 ]; then
+		echo -e '\e[01;32m:)'
+	else
+		echo -e '\e[01;31m:('
+	fi
+}
+
+GIT_PS1_SHOWDIRTYSTATE=true
+chars=("🐷" "🐯" "🐸" "🐶" "🐵" "🐙" "🐮" "🐰" "🐻" "🐱" "🐯" "🐨" "🐭")
+num_chars=${#chars[*]}
+COOL_CHAR="${chars[$((RANDOM%num_chars))]} "
+export PS1="\[$CYAN\]"'\u'"\[$YELLOW\]@\[$GRAY\]"'\h'"\[$WHITEb\] \w \[$RED\]"'$(gitbranch)'"\n\[$DEFAULT\]$COOL_CHAR "
